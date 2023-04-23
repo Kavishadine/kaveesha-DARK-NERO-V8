82caacbc6f390c292c5896d55fa2770cdc458add
Kavishadine <116986965+Kavishadine@users.noreply.github.com>
2023-04-23 07:26:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
+global.session = " /VjJpUmVDalg=session Id
 global.ownernomer = "94712351830" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94712351830'] //ur premium number
