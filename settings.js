07dc812485139986e64417cf25567b7e1bdc1658
Kavishadine <116986965+Kavishadine@users.noreply.github.com>
2023-04-23 08:14:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
 global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0712351830'] //ur owner number
+global.owner = ['0789968177'] //ur owner number
 global.ownername = "kaveesha dineth" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
@@ -28,9 +28,9 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.session = " /VjJpUmVDalg=session Id
-global.ownernomer = "94712351830" //ur number
+global.ownernomer = "94789968177" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94712351830'] //ur premium number
+global.premium = ['94789968177'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
